ea573beceea7ce6e51c3f2066025090fea492a7a
Uncle Pedro <you@example.com>
2023-10-28 11:45:10 +1000

    Add styling and additional counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react';
-import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const getTotal = () => {
+    return count1 + count2;
+  };
 
   return (
-    <div className={''}>
+    <div className={'flex gap-4 flex-col m-4'}>
       <h1 className={'text-2xl'}>Vite + React + TS + Tailwind</h1>
-      <div>
-        <button className={'outline'} onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex gap-6">
+        <button
+          className={'outline'}
+          onClick={() => {
+            setCount1(count1 + 1);
+          }}
+        >
+          count is {count1}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className={'outline'} onClick={() => setCount2(count2 + 1)}>
+          count is {count2}
+        </button>
+        <p>Total is {getTotal()}</p>
       </div>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
     </div>
   );
 }
