1ea0ac5e258da13e23a2b51c6da8747ac202f595
Uncle Pedro <you@example.com>
2024-01-14 14:07:38 +1000

    Improve pages functionality:  - Read URL in order to display encrypter/decrypter  - Import encrypt/decrypt function into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
+import InputSection from './components/InputSection';
 import Dashboard from './screens/Dashboard';
+import { decrypt, encrypt } from './helpers/encryption';
 
 function App() {
+  console.log('window.location.pathname', window.location.pathname);
+  const [userInput, setUserInput] = useState<string>('');
+  const encryptedUserInput = encrypt(userInput);
+  const decryptedUserInput = decrypt(userInput);
+
+  // Return the relevant screen based on the path
+  const renderScreen = () => {
+    const pathname = window.location.pathname;
+
+    if (pathname === '/encrypt') {
+      return (
+        <InputSection
+          title={'Encrypt'}
+          onChange={(updatedInput) => setUserInput(updatedInput)}
+          output={encryptedUserInput}
+        />
+      );
+    } else if (pathname === '/decrypt') {
+      return (
+        <InputSection
+          title={'Decrypt'}
+          onChange={(updatedInput) => setUserInput(updatedInput)}
+          output={decryptedUserInput}
+        />
+      );
+    } else {
+      return <Dashboard />;
+    }
+
+    // switch (window.location.pathname) {
+    //   case encrypt:
+    //     url = window.location.pathname = '/encrypt'
+    //     break;
+    //   default:
+    //     return <Dashboard />;
+    // }
+  };
+
   return (
     <>
       <div>
         <Banner />
       </div>
-      <div className={'flex items-center justify-center gap-4 flex-col h-screen '}>
-        <Dashboard />
-      </div>
+      <div className={'flex items-center justify-center gap-4 flex-col h-screen'}>{renderScreen()}</div>
     </>
   );
 }
