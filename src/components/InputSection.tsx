fc9c46274d984917573b48a2a18ada260a037185
Uncle Pedro <you@example.com>
2024-03-28 10:50:40 +1000

    Adjust input section for mobile

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -8,7 +8,7 @@ const InputSection = ({ onChange, title, output }: Props) => {
   return (
     <>
       <h2 className={'text-2xl font-mono text-lime-400 bg-black p-4 w-fit'}>{title}</h2>
-      <div className="p-4 bg-black text-lime-400 flex gap-2 w-1/4 flex-col border-lime-500 border-2">
+      <div className="w-[300px] md:w-[500px] p-4 bg-black text-lime-400 flex gap-2 flex-col border-lime-500 border-2">
         <div className="flex flex-row gap-2">
           <p className="text-green-800 animate-pulse">{'>'}</p>
           <input
