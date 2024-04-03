04ff91eee8c83493f0d88a89fef749412c2ea896
Uncle Pedro <peterforsyth10@gmail.com>
2024-04-03 14:01:25 +1000

    Adjust buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,10 @@ interface Props {
 const Button = ({ title, onClick }: Props) => {
   return (
     <>
-      <button className={'text-2xl font-mono text-lime-400 bg-black p-4 w-fit'} onClick={() => onClick()}>
+      <button
+        className={'text-2xl font-mono text-lime-400 bg-black p-4 w-fit rounded-lg duration-300  hover:opacity-90'}
+        onClick={() => onClick()}
+      >
         {title}
       </button>
     </>
